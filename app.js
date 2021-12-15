250b483ec95d728d39e36a9246a4884b589383f2
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2021-12-15 17:52:15 +0530

    mock data added

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ var app = require('express')();
 var morgan = require('morgan');
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
-var promise = mongoose.connect('mongodb://localhost/mydb', {useMongoClient: true});
+var mysql = require('mysql');
+var promise = mongoose.connect('mongodb://localhost/mydb');
+var mysql_connect = mysql.createConnection({host: 'localhost', user:'manu', password: '1234', database: 'society'});
 var router = require('./app/route/approute');
-const societyrouter = require('./app/route/societyroute');
+// const societyrouter = require('./app/route/societyroute');
 const apiRoute = require('./app/route/apiroute')
 var compression = require('compression');
 var helmet = require('helmet');
-const passport = require('passport');
+// const passport = require('passport');
 const authenticate = require('./app/authentication');
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use(compression());
 app.use(helmet());
@@ -28,11 +30,26 @@ app.use(function(req,res,next){
 });
 
 app.get('/',(req,res)=>{
-    res.status(200).json(process.env);
+    res.status(200).json('process.env');
+});
+
+app.get('/dbconn', (req,res) => {
+    mysql_connect.connect(function(err) {  
+        if (err) throw err;  
+        console.log("Connected!");  
+      });  
+    mysql_connect.query('select count(*) form test', (err, result) => {
+        if(err)
+            console.log(err);
+        
+        console.log('result', result);
+        const conn = true;
+        res.status(200).json({connect: result});
+    });
 });
 
 //app.use('/',router);
-app.use('/society',societyrouter);
+// app.use('/society',societyrouter);
 app.use('/api',apiRoute)
 
 app.listen(3000,()=>{
